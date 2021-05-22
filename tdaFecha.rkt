5a32f5afb0c31407b85165f048f2576b3b58744e
Cristhofer Parada <cristhofer.parada@usach.cl>
2021-05-21 22:11:30 -0400

    Update tdaFecha.rkt

--- tdaFecha.rkt
+++ tdaFecha.rkt
@@ -7,11 +7,13 @@
 ;Descripcion: Crea una fecha / posibles necesidades: verificar años bisiestos, cuales meses tienen 31 dias y que febrero tiene 28 dias
 ;Dominio: (integer X integer X integer)
 ;Recorrido: list
-(define (crearFecha day month year)
-  (if (and(integer? day)(integer? month)(integer? year)(> day 0)(< day 32)(> month 0)(< month 13)(> year 0))
-          (list day month year)
-          null
-          )
+(define crearFecha
+  (lambda (day month year)
+    (if (and(integer? day)(integer? month)(integer? year)(> day 0)(< day 32)(> month 0)(< month 13)(> year 0))
+        (list day month year)
+         null
+         )
+    )
   )
 
 
@@ -20,40 +22,48 @@
 ;descripción: Funcion que evalua si la entrada pertenece al TDA fecha
 ;dom: elemento de cualquier tipo
 ;rec: boolean
-(define (esFecha? entrada)
-  (and (list? entrada) 
-       (= (length entrada) 3)
-       (not (null? (crearFecha (car entrada) (cadr entrada) (caddr entrada)))))
-)
+(define esFecha?
+  (lambda (entrada)
+    (and (list? entrada)
+         (= (length entrada) 3)
+         (not (null? (crearFecha (car entrada) (cadr entrada) (caddr entrada)))))
+    )
+  )
 
 ;SELECTORES
 
 ;Descripcion: Funcion que busca obtener el dia de una fecha
 ;Dominio: TDA Fecha
 ;Recorrido: integer
-(define (getDay entrada)
-  (if (esFecha? entrada)
-      (car entrada)
-      0
-      )
+(define getDay
+  (lambda (entrada)
+    (if (esFecha? entrada)
+        (car entrada)
+        0
+        )
+    )
   )
 ;Descripcion: Funcion que busca obtener el mes de una fecha
 ;Dominio: TDA Fecha
 ;Recorrido: integer
-(define (getMonth entrada)
-  (if (esFecha? entrada)
-      (cadr entrada)
-      0
-      )
+(define getMonth
+  (lambda (entrada)
+    (if (esFecha? entrada)
+        (cadr entrada)
+        0
+        )
+    )
   )
 ;Descripcion: Funcion que busca obtener el año de una fecha
 ;Dominio: TDA Fecha
 ;Recorrido: integer
-(define (getYear entrada)
+(define getYear
+  (lambda (entrada)
   (if (esFecha? entrada)
       (caddr entrada)
       0
       )
+    )
   )
   
 ; MODIFICADORES
@@ -61,29 +71,36 @@
 ;Descripcion: Crea una lista nueva a partir de la entregada modificando el dato de dia
 ;Dominio: fecha x integer
 ;Recorrido: fecha
-(define(changeDay entrada newDay)
-  (if (esFecha? entrada)
-      (crearFecha newDay (getMonth entrada) (getYear entrada))
-      null
-      )
+(define changeDay
+  (lambda (entrada newDay)
+    (if (esFecha? entrada)
+        (crearFecha newDay (getMonth entrada) (getYear entrada))
+        null
+        )
+    )
   )
 
 ;Descripcion: Crea una lista nueva a partir de la entregada modificando el dato de mes
 ;Dominio: fecha x integer
 ;Recorrido: fecha
-(define(changeMonth entrada newMonth)
-  (if (esFecha? entrada)
-      (crearFecha (getDay entrada) newMonth (getYear entrada))
-      null
+(define changeMonth
+  (lambda (entrada newMonth)
+    (if (esFecha? entrada)
+        (crearFecha (getDay entrada) newMonth (getYear entrada))
+        null
       )
+    )
   )
 
 ;Descripcion: Crea una lista nueva a partir de la entregada modificando el dato de mes
 ;Dominio: fecha x integer
 ;Recorrido: fecha
-(define(changeYear entrada newYear)
-  (if (esFecha? entrada)
-      (crearFecha (getDay entrada) (getMonth entrada) newYear)
-      null
-      )
+(define changeYear
+  (lambda (entrada newYear)
+    (if (esFecha? entrada)
+        (crearFecha (getDay entrada) (getMonth entrada) newYear)
+        null
+        )
+    )
   )
+
